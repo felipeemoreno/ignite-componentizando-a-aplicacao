ccac871ad46c689489b0ecef93a5e49069483bb0
felipeemoreno <contato@w5server.com.br>
2021-09-18 18:29:14 -0300

    Completed

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,41 @@
-export function Content() {
-  // Complete aqui
+import { Header } from './Header';
+import { MovieCard } from './MovieCard';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  selectedGenre: GenreResponseProps;
+  moviesList: MovieProps[];
+}
+export function Content(props: ContentProps) {
+
+  const { selectedGenre, moviesList: movies } = props;
+  return (
+    <div className="container">
+       <Header selectedGenre={selectedGenre} />
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+interface HeaderPros {
+    selectedGenre: {
+        title: string;
+    }
+}
+export function Header(props: HeaderPros) {
+
+  const { selectedGenre } = props;
+  
+  return (
+    <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+    </header>
+    )
+}
